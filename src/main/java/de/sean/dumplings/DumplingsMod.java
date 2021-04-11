4da3d92a3cb469012f03dd6f83979426fc711e3d
pnda <43609023+ThePNDA@users.noreply.github.com>
2021-04-11 20:33:51 +0200

    Add: Bread dumplings

--- src/main/java/de/sean/dumplings/DumplingsMod.java
+++ src/main/java/de/sean/dumplings/DumplingsMod.java
@@ -11,15 +11,19 @@ import net.minecraft.util.registry.Registry;
 public class DumplingsMod implements ModInitializer {
     public static final String MODID = "dumplings";
 
-    private static final FoodComponent DUMPLINGS_FOOD = (new FoodComponent.Builder()).hunger(4).saturationModifier(0.3F).build();
+    private static final FoodComponent DUMPLINGS_FOOD = (new FoodComponent.Builder()).hunger(6).saturationModifier(0.6F).build();
     private static final FoodComponent DUMPLINGS_WITH_CHICKEN_FOOD = (new FoodComponent.Builder()).hunger(12).saturationModifier(0.8F).meat().build();
 
     public static final Item DUMPLINGS_ITEM = new Item(new FabricItemSettings().food(DUMPLINGS_FOOD).group(ItemGroup.FOOD));
+	public static final Item BREAD_DUMPLINGS_ITEM = new Item(new FabricItemSettings().food(DUMPLINGS_FOOD).group(ItemGroup.FOOD));
+
     public static final Item DUMPLINGS_WITH_CHICKEN_ITEM = new Item(new FabricItemSettings().food(DUMPLINGS_WITH_CHICKEN_FOOD).group(ItemGroup.FOOD));
 
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier(MODID, "dumplings"), DUMPLINGS_ITEM);
+		Registry.register(Registry.ITEM, new Identifier(MODID, "bread_dumplings"), BREAD_DUMPLINGS_ITEM);
+
         Registry.register(Registry.ITEM, new Identifier(MODID, "dumplings_with_chicken"), DUMPLINGS_WITH_CHICKEN_ITEM);
     }
 }
